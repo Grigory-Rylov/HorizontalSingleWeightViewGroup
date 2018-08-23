e53c0e05d5646307fda9517e21f286e52eca2701
Grigory Rylov <grishberg@gmail.com>
2018-08-23 22:15:09 +0300

    added transition animation

--- sample-app/src/main/java/com/grishberg/barviewgroup/MainActivity.kt
+++ sample-app/src/main/java/com/grishberg/barviewgroup/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.grishberg.barviewgroup
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import com.grishberg.strategyviewgroup.BarLayoutUpdater
 import com.grishberg.strategyviewgroup.BarViewGroup
 
@@ -17,15 +21,27 @@ class MainActivity : AppCompatActivity() {
         val oneRowLayoutUpdater = BarLayoutUpdater(this, R.layout.one_row_bar)
         val twoRowsLayoutUpdater = BarLayoutUpdater(this, R.layout.two_row_bar)
 
-        val rootView = findViewById<View>(R.id.root)
+        val rootView = findViewById<LinearLayout>(R.id.root)
         val barView = findViewById<BarViewGroup>(R.id.barView)
-
+        val button1 = findViewById<ImageButton>(R.id.button2)
+        val button5 = findViewById<ImageButton>(R.id.button3)
+        twoRowsLayoutUpdater.applyTo(barView)
         rootView.setOnClickListener {
             twoRow = !twoRow
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                TransitionManager.beginDelayedTransition(barView)
+            }
+            barView.removeView(button1)
+            barView.removeView(button5)
             if (twoRow) {
+
+                barView.addView(button1,2)
+                barView.addView(button5, 1)
                 twoRowsLayoutUpdater.applyTo(barView)
             } else {
                 oneRowLayoutUpdater.applyTo(barView)
+                barView.addView(button1, 1)
+                barView.addView(button5, 2)
             }
         }
     }
